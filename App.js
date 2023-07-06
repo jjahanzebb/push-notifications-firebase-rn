cdbba849ee7b9774f4b5d269b8e7e1b331039bf7
Jahan Zeb Ahmed <jjahanzebb@gmail.com>
2023-07-06 17:22:41 +0500

    Day 1.0 - Basic project set up to receiv notifications

--- App.js
+++ App.js
@@ -1,59 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Alert } from "react-native";
+import { StyleSheet, Text, View, Alert, Button } from "react-native";
 import React, { useEffect } from "react";
-import messaging from '@react-native-firebase/messaging';
-
+import messaging from "@react-native-firebase/messaging";
+import {
+  getToken,
+  notificationListener,
+  onDisplayNotification,
+  requestUserPermission,
+} from "./utils";
 
 export default function App({ navigation }) {
-  const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    if (enabled) {
-      console.log("Authorization status:", authStatus);
-    }
-  };
-
   useEffect(() => {
-    if (requestUserPermission()) {
-      // return fcm token for device
-
-      messaging()
-        .getToken()
-        .then((token) => {
-          console.log(token);
-        });
-    } else {
-      console.log("Failed to get token status", authStatus);
-    }
-
-    // Check whether an initial notification is available
-    messaging()
-      .getInitialNotification()
-      .then(async (remoteMessage) => {
-        if (remoteMessage) {
-          console.log(
-            "Notification caused app to open from quit state:",
-            remoteMessage.notification
-          );
-        }
-      });
-
-    // Assume a message-notification contains a "type" property in the data payload of the screen to open
-    messaging().onNotificationOpenedApp(async (remoteMessage) => {
-      console.log(
-        "Notification caused app to open from background state:",
-        remoteMessage.notification
-      );
-    });
-
-    // Register background handler
-    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-      console.log("Message handled in the background!", remoteMessage);
-    });
-
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
       Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
     });
@@ -61,10 +18,36 @@ export default function App({ navigation }) {
     return unsubscribe;
   }, []);
 
+  useEffect(() => {
+    requestUserPermission();
+    notificationListener();
+    getToken();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>FCM Test Application</Text>
       <StatusBar style="auto" />
+
+      <Text>FCM Test Application</Text>
+      <Button
+        title="ALERT LOCAL NOTIFY 2"
+        onPress={() =>
+          onDisplayNotification({
+            title: "Notification Title",
+            body: "Main body content of the notification",
+          })
+        }
+      />
+
+      <Button
+        title="ALERT LOCAL NOTIFY 2"
+        onPress={() =>
+          onDisplayNotification({
+            title: "Notification Title 2",
+            body: "Main body content of the notification 2",
+          })
+        }
+      />
     </View>
   );
 }
---
+++ utils.js
@@ -0,0 +1,68 @@
+import messaging from "@react-native-firebase/messaging";
+import notifee from "@notifee/react-native";
+
+export const requestUserPermission = async () => {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log("Authorization status:", authStatus);
+  }
+};
+
+export const notificationListener = async () => {
+  // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+  messaging().onNotificationOpenedApp((remoteMessage) => {
+    console.log(
+      "Notification caused app to open from background state:",
+      remoteMessage.notification
+    );
+  });
+
+  // Check whether an initial notification is available
+  messaging()
+    .getInitialNotification()
+    .then((remoteMessage) => {
+      if (remoteMessage) {
+        console.log(
+          "Notification caused app to open from quit state:",
+          remoteMessage.notification
+        );
+      }
+    });
+};
+
+export const getToken = async () => {
+  await messaging().registerDeviceForRemoteMessages();
+  const token = await messaging().getToken();
+
+  console.log("Token =>", token);
+};
+
+export const onDisplayNotification = async () => {
+  // Request permissions (required for iOS)
+  await notifee.requestPermission();
+
+  // Create a channel (required for Android)
+  const channelId = await notifee.createChannel({
+    id: "default",
+    name: "Default Channel",
+  });
+
+  // Display a notification
+  await notifee.displayNotification({
+    title: "Notification Title",
+    body: "Main body content of the notification",
+    android: {
+      channelId,
+      smallIcon: "name-of-a-small-icon", // optional, defaults to 'ic_launcher'.
+      // pressAction is needed if you want the notification to open the app when pressed
+      pressAction: {
+        id: "default",
+      },
+    },
+  });
+};
